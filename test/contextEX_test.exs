dceb2488ae95d1cd113b191d98a40e7ec0bf959a
MizukiNakano <mizuki590@gmail.com>
2016-07-14 22:25:05 +0900

    layered function test

--- test/contextEX_test.exs
+++ test/contextEX_test.exs
@@ -20,9 +20,17 @@ defmodule ContextEXTest do
         {:getLayer, caller} ->
           send caller, getActiveLayers
           routine
+        {:func, caller} ->
+          send caller, func
+          routine
         {:end, caller} -> send caller, :end
       end
     end
+
+    deflf func(), %{:groupA => :layer1, :groupB => :layer2}, do: 2
+    deflf func(), %{:groupB => :layer3}, do: 3
+    deflf func(), %{:groupA => :layer1}, do: 1
+    deflf func(), %{}, do: 0
   end
 
 
@@ -75,6 +83,32 @@ defmodule ContextEXTest do
     send p1, {:getLayer, self}
     receive do
       result -> assert result = %{:groupA => :layer1}
+    end
+  end
+
+  test "layered function test" do
+    p = Caller.start
+    send p, {:func, self}
+    receive do
+      result -> assert result == 0
+    end
+
+    send p, {:activate, %{:groupA => :layer1}}
+    send p, {:func, self}
+    receive do
+      result -> assert result == 1
+    end
+
+    send p, {:activate, %{:groupB => :layer2}}
+    send p, {:func, self}
+    receive do
+      result -> assert result == 2
+    end
+
+    send p, {:activate, %{:groupB => :layer3}}
+    send p, {:func, self}
+    receive do
+      result -> assert result == 3
     end
   end
 end
