e1ff6312a0c78f1151815d49c435eb281349e01d
MizukiNakano <mizuki590@gmail.com>
2017-01-09 18:17:06 +0900

    version up

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ContextEX.Mixfile do
 
   def project do
     [app: :contextEX,
-     version: "0.3.4",
+     version: "0.3.5",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
